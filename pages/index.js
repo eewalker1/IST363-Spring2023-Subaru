c21c85e5c91988871fa55c298166331b36eea2c3
eewalker1 <eewalker@syr.edu>
2023-03-28 18:21:31 -0400

    car color picker

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 
 import Button from '../components/Button';
+import CarColorPicker from '../components/CarColorPicker';
 import Header from '../components/Header';
 import Image from 'next/image';
 import Layout from '../components/Layout';
 import Swatch from '../components/Swatch';
 
 import { getAllVehicles, getAllColors } from '../lib/api';
+
 
 export async function getStaticProps() {
   // Get external data from the file system, API, DB, etc.
@@ -27,16 +29,7 @@ const Homepage = ({data, colors}) => {
   //console.log({data});
   return <Layout>
     <h1>Homepage</h1>
-    <h2>Color picker</h2>
-    <div>
-      Large image goes here.
-    </div>
-    <ul>
-      {colors.map((color) => {
-        return <Swatch color={color} />
-      })}
-    </ul>
-    <h3>Color name</h3>
+    <CarColorPicker colors={colors} />
   </Layout>
 }
 export default Homepage 
